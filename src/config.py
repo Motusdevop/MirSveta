055e38ff1b6c194342ea5a6f9129916039df42d2
Motusdevop <motusdevop@gmail.com>
2025-03-16 20:48:46 +0300

    fix

--- src/config.py
+++ src/config.py
@@ -7,8 +7,8 @@ load_dotenv("bot.env")
 TOKEN = os.getenv("TOKEN")
 
 
-CATEGORIES_PATH = "categories.json"
-MESSAGES_PATH = "messages.json"
+CATEGORIES_PATH = "../categories.json"
+MESSAGES_PATH = "../messages.json"
 
 admins = [
     700178752,
